310dc37caa1077c1aff8559e193dde970d358dec
Остап Беляев <o.belyaev@vk.team>
2024-06-12 14:48:08 +0300

    add negative test to delete image

---
+++ test-glance/test-negative-delete-image.py
@@ -0,0 +1,19 @@
+import pytest
+from base import get_image_info, get_token, delete_image
+import configparser
+
+config = configparser.ConfigParser()
+config.read("/home/ostap/PycharmProjects/openstack-testing/config.ini")
+
+image_id = "non-existing-id"
+
+
+def test_negative_delete_image():
+    try:
+        token = get_token()
+
+        delete_response = delete_image(token, image_id)
+        assert delete_response.status_code == 404
+
+    except Exception as e:
+        pytest.fail(f"Test failed: {e}")
