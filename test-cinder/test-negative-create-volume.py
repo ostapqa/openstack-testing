c3a20f44553994aab88ea83fc9f57629b9932f9e
Ostap Belyaev <ostap.belyaev@yandex.ru>
2024-06-12 15:10:29 +0300

    add negative test to create volume

--- test-cinder/test-negative-create-volume.py
+++ test-cinder/test-negative-create-volume.py
@@ -4,19 +4,12 @@ from base import get_token, create_volume
 def test_create_volume():
     try:
         token = get_token()
-        volume_name = "test-volume"
+        volume_name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
         volume_size = 1
         volume_description = "Test volume description"
 
         response = create_volume(token, volume_name, volume_size, volume_description)
-        assert response.status_code == 202
-
-        volume = response.json().get("volume", {})
-        assert volume.get("name") == volume_name
-        assert volume.get("size") == volume_size
-        assert volume.get("description") == volume_description
-
-        print(f"Volume created successfully: {volume}")
+        assert response.status_code == 400
 
     except Exception as e:
         pytest.fail(f"Test failed: {e}")
