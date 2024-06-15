2e9eb26f8e221c4f1cd86b9eb5bf05b43c559877
Остап Беляев <o.belyaev@vk.team>
2024-06-15 15:41:47 +0300

    add negative and positive tests to delete a volume

---
+++ test-cinder/test-negative-delete-volume.py
@@ -0,0 +1,17 @@
+import time
+import pytest
+
+from base import delete_volume, get_token
+
+
+def test_delete_volume():
+    try:
+        token = get_token()
+
+        volume_id = 'fake-volume-id'
+
+        delete_response = delete_volume(token, volume_id)
+        assert delete_response.status_code == 404
+
+    except Exception as e:
+        pytest.fail(f"Test failed: {e}")
