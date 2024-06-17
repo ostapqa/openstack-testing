dd0525349fb433936f884a6310000976bbd446b1
Остап Беляев <o.belyaev@vk.team>
2024-06-17 17:53:31 +0300

    refactor nova tests

--- test-nova/test-delete-vm.py
+++ test-nova/test-delete-vm.py
@@ -5,16 +5,17 @@ import configparser
 config = configparser.ConfigParser()
 config.read("/home/ostap/PycharmProjects/openstack-testing/config.ini")
 
-image_id = config.get('compute', 'image')
-flavor_id = config.get('compute', 'flavor')
-network_id = config.get('compute', 'network')
-
 
 def test_delete_server():
     try:
         token = get_token()
+        image_id = config.get('compute', 'image')
+        flavor_id = config.get('compute', 'flavor')
+        network_id = config.get('compute', 'network')
         server_name = 'test-server'
+
         server = create_server(token, server_name, image_id, flavor_id, network_id)
+        assert server.status_code == 202
         delete_server_by_name(server_name, token)
     except Exception as e:
         pytest.fail(f"Failed to delete server '{server_name}': {e}")
--- test-nova/test-get-vm.py
+++ test-nova/test-get-vm.py
@@ -1,22 +1,36 @@
-from base import get_server_info, get_token
+import time
+
+from base import get_server_info, get_token, create_server, delete_server
 import pytest
 import configparser
 
 config = configparser.ConfigParser()
 config.read("/home/ostap/PycharmProjects/openstack-testing/config.ini")
-server_name = config.get('compute', 'server_name')
 
 
 def test_get_server_info():
     try:
         token = get_token()
+        server_name = "test-server"
+        image_id = config.get('compute', 'image')
+        flavor_id = config.get('compute', 'flavor')
+        network_id = config.get('compute', 'network')
 
-        server = get_server_info(server_name, token)
+        create_response = create_server(token, server_name, image_id, flavor_id, network_id)
+        assert create_response.status_code == 202
+        server_id = create_response.json().get("server", {}).get("id")
+        assert server_id is not None
+        server_id = create_response.json().get("server", {}).get("id")
+
+        time.sleep(10)
+        server = get_server_info(token, server_id)
         server_info = server.json()["server"]
 
         assert server.status_code is 200
         assert server_info["name"] == server_name
         assert server_info["status"] in ["ACTIVE"]
         print(f"Server info retrieved successfully: {server_info['id']}")
+
+        delete_server(token, server_id)
     except Exception as e:
         pytest.fail(f"Test failed: {e}")
